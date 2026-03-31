8aa3bce46e30f8b9f98d99db3756284f80f1b9aa
Prempal Singh <prempalsingh030499@gmail.com>
2026-03-31 16:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function Employee(name, age, jobTitle) {
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
+// Creating an instance of Person
+const person = new Person("Alice", 25);
+person.greet();
+// Expected Output: Hello, my name is Alice, I am 25 years old.
+
+// Creating an instance of Employee
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
+// Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager
