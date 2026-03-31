9e3a3b52b476214d948f2f1a0c66241cb0342c3c
Prempal Singh <prempalsingh030499@gmail.com>
2026-03-31 17:06:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function Person(name, age) {
 	this.age = age;
 	}
 	greet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
 }
 
-function Employee(name, age, jobTitle) {
-	constructor(jobTitle){
+function extends Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
 		super(name,age);
 		this.jobTitle = jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 
